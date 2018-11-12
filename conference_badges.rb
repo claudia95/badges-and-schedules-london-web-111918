675f0ec5955a747a7dff4ff75b12e9f01f5de750
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 23:44:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(attendees)
   room_assignment = []
   attendees.each_with_index do |attendees, index|
-    room_assignment.unshift("Hello, #{attendees}! You'll be assigned to room #{index}!")
+    room_assignment.unshift("Hello, #{attendees}! You'll be assigned to room #{index + 1}!")
   end
   return room_assignment
 end
