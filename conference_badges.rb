e4ecd5f528ac805e867b88b09bc0b028e41e5b61
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 23:23:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,17 @@
 # Write your code here.
 
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
